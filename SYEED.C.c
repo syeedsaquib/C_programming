4a2e60434aca01405b000a1539932180843bc124
Syeed Mohd Saquib <101272076+syeedsaquib@users.noreply.github.com>
2022-04-29 19:58:14 +0530

    Add files via upload

---
+++ SYEED.C.c
@@ -0,0 +1,144 @@
+#include<stdio.h>
+int main()
+{
+										      	/*     program for "S"    */
+						    	
+	int i,j,k,l,m,n,o;
+	
+	for (i=1;i<=4;i++)
+	{
+		printf("= ");
+	}
+	for(j=1;j<=4;j++)
+	{
+		printf("=\n");
+	}
+	for (k=1;k<=5;k++)
+	{
+		printf("= ");
+	}
+	for(l=1;l<=3;l++)
+	{
+		printf("\n        ");
+		for(m=1;m<=1;m++)
+		{
+			printf("=");
+		}
+	}
+		printf("\n");
+		for (i=1;i<=5;i++)
+	{
+		printf("= ");
+	}
+	printf("\n\n");
+												/*    program for "Y"   */
+	
+	int a,b,c,d,e,f,g,h;
+	
+	for(a=1;a<=1;a++)
+	{
+		printf("=");
+		for(b=1;b<=7;b++)
+		{
+			printf(" ");
+		}
+		printf("=\n");
+	}
+	for(c=1;c<=1;c++)
+	{
+		printf(" =");
+		for(d=1;d<=5;d++)
+		{
+			printf(" ");
+		}
+		printf("=\n");
+	}
+	for(e=1;e<=1;e++)
+	{
+		printf("  =");
+		for(f=1;f<=3;f++)
+		{
+			printf(" ");
+		}
+		printf("=\n");
+	}
+	for(g=1;g<=4;g++)
+	{
+		printf("    =\n");
+	}
+	printf("\n\n");
+                                         	/*      program for "E"     */
+	
+	
+	for (i=1;i<=4;i++)
+	{
+		printf("= ");
+	}
+	for(j=1;j<=4;j++)
+	{
+		printf("=\n");
+	}
+	for (k=1;k<=5;k++)
+	{
+		printf("= ");
+	}
+	for(l=1;l<=3;l++)
+	{
+		printf("\n");
+		for(m=1;m<=1;m++)
+		{
+			printf("=");
+		}
+	}
+		printf("\n");
+		for (i=1;i<=5;i++)
+	{
+		printf("= ");
+	}
+	printf("\n\n");
+	                                            /*      program for "E"      */
+	for (i=1;i<=4;i++)
+	{
+		printf("= ");
+	}
+	for(j=1;j<=4;j++)
+	{
+		printf("=\n");
+	}
+	for (k=1;k<=5;k++)
+	{
+		printf("= ");
+	}
+	for(l=1;l<=3;l++)
+	{
+		printf("\n");
+		for(m=1;m<=1;m++)
+		{
+			printf("=");
+		}
+	}
+		printf("\n");
+		for (i=1;i<=5;i++)
+	{
+		printf("= ");
+	}
+	printf("\n\n");
+														/*      program for "D"      */
+	
+	for(i=1;i<=1;i++)
+	{
+		for(k=1;k<=5;k++)
+		{
+			printf("= ");
+		}
+		for(j=1;j<=5;j++)
+		{
+			printf("\n=        =");
+		}
+		printf("\n");
+		for(k=1;k<=5;k++)
+		{
+			printf("= ");
+		}
+	}
+}
